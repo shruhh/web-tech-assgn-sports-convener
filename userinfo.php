6b575ed6590251b10b13e7a58fd359d7d8de6c54
shruhh <62129072+shruhh@users.noreply.github.com>
2020-05-26 17:46:19 +0530

    updated userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -8,4 +8,16 @@ if($con){
 else{
 	echo "No connection!";
 }
+
+mysqli_select_db($con, 'iitbsportsdata');
+
+$name = $_POST['name'];
+$roll = $_POST['roll'];
+
+$query = "insert into userinfodata (name, roll) values ('$name', '$roll')" ;
+
+mysqli_query($con, $query);
+
+header('location:index.php');
+
 ?>
